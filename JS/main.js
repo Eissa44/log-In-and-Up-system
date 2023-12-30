3e1df60424b0b35cc77d9f2667702d7dde7c35c1
Eissa44 <eissaelemam44@gmail.com>
2023-12-30 20:55:41 +0200

    that's a html and css design

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// start of region catch any value the user entered
+let inEmail = document.getElementById("signinEmail");
+let inPassword = document.getElementById("signinPassword");
+// end of region
+
+// start of region catch my signin button
+let login = document.getElementById("loginBtn");
+login.addEventListener("click", signin);
+// end of region
+// start of region an empaty array to store our values
+var box = [];
+// end of region
+
+function signin() {
+  let data = {};
+}
