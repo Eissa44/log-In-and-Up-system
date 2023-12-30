7a6fa5b800189b39d1ffed81e8aea891db963a20
Eissa44 <eissaelemam44@gmail.com>
2023-12-30 23:04:34 +0200

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,31 @@
 // start of region catch any value the user entered
-let inEmail = document.getElementById("signinEmail");
-let inPassword = document.getElementById("signinPassword");
+
+// let inEmail = document.getElementById("signupName");
+// let inPassword = document.getElementById("signupName");
+let upName = document.getElementById("signupName");
+let upEmail = document.getElementById("signupEmail");
+let upPassword = document.getElementById("signupPassword");
 // end of region
 
 // start of region catch my signin button
-let login = document.getElementById("loginBtn");
-login.addEventListener("click", signin);
+// let logUp = document.getElementById("logUpBtn");
+// logUp.addEventListener("click", signup);
 // end of region
 // start of region an empaty array to store our values
 var box = [];
 // end of region
 
-function signin() {
-  let data = {};
+function signup() {
+  if (upName.value !== "" && upEmail.value !== "" && upPassword.value !== "") {
+    let data = {
+      pname: upName.value,
+      pmail: upEmail.value,
+      paas: upPassword.value,
+    };
+    box.push(data);
+    localStorage.setItem("items", JSON.stringify(box));
+    document.getElementById("errorMassage").innerHTML = `Secces`;
+  } else {
+    document.getElementById("errorMassage").innerHTML = `Enter Value`;
+  }
 }
