1f5b87eebe2debba9bea44ea2446e75d21440b98
Eissa44 <eissaelemam44@gmail.com>
2023-12-31 08:26:41 +0200

    third update

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,12 @@
 // start of region catch any value the user entered
 
-// let inEmail = document.getElementById("signupName");
-// let inPassword = document.getElementById("signupName");
+let inEmail = document.getElementById("signupName");
+let inPassword = document.getElementById("signupName");
 let upName = document.getElementById("signupName");
 let upEmail = document.getElementById("signupEmail");
 let upPassword = document.getElementById("signupPassword");
 // end of region
 
-// start of region catch my signin button
-// let logUp = document.getElementById("logUpBtn");
-// logUp.addEventListener("click", signup);
-// end of region
 // start of region an empaty array to store our values
 var box = [];
 // end of region
@@ -20,7 +16,7 @@ function signup() {
     let data = {
       pname: upName.value,
       pmail: upEmail.value,
-      paas: upPassword.value,
+      pass: upPassword.value,
     };
     box.push(data);
     localStorage.setItem("items", JSON.stringify(box));
@@ -29,3 +25,32 @@ function signup() {
     document.getElementById("errorMassage").innerHTML = `Enter Value`;
   }
 }
+
+var cartona = JSON.parse(localStorage.getItem("items"));
+var i = 0;
+
+function login() {
+  for (i = 0; i < cartona.length; i++) {
+    if (
+      cartona &&
+      cartona[i].pmail === inEmail.value &&
+      cartona[i].pass === inPassword.value
+    ) {
+      localStorage.setItem("name", JSON.stringify(cartona[i].pname));
+      window.location.href = "./welcome.html?";
+      console.log(cartona[i].pname);
+      return;
+    } else {
+      document.getElementById("incorrect").innerHTML = `No Acc`;
+    }
+  }
+}
+
+function welcom() {
+  var welc = document.getElementById("welcome");
+  welc.innerHTML = `welcome ${JSON.parse(localStorage.getItem("name"))} `;
+}
+// start of region catch my signin button
+// let logUp = document.getElementById("logUpBtn");
+// logUp.addEventListener("click", signup);
+// end of region
